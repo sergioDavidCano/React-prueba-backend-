c1bec11d1ab41e23bb2955b93595d3b7b499d45d
SergioCano <sergio0201david@gmail.com>
2021-10-13 08:50:18 -0500

    auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -89,6 +89,8 @@ const revalidarToken = async(req, res = response) => {
 
     res.json({
         ok: true,
+        uid,
+        name,
         token
     })
 }
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const { dbConnection } = require('./database/config');
-
 // Crear el servidor de express
 const app = express();
-
+app.use(bodyParser.json());
 // Base de datos
 dbConnection();
 
@@ -13,25 +13,19 @@ dbConnection();
 app.use(cors())
 
 // Directorio Público
-app.use( express.static('public') );
+app.use(express.static('public'));
 
 // Lectura y parseo del body
-app.use( express.json() );
+app.use(express.json());
 
 // Rutas
-app.use('/api/auth', require('./routes/auth') );
-app.use('/api/events', require('./routes/events') );
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
 
 
 
 
 // Escuchar peticiones
-app.listen( process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 });
-
-
-
-
-
-
